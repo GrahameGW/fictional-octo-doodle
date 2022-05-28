c3731b53182494d084031660c411a988a71f7045
Grahame Watt <watt.grah@gmail.com>
2022-05-28 12:33:35 -0700

    goop bullets

---
+++ Assets/Scripts/Enemies/GoopBullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace FictionalOctoDoodle.Core
+{
+    public class GoopBullet : MonoBehaviour
+    {
+        [SerializeField] int damage;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var player = collision.gameObject.GetComponentInParent<Player>();
+            if (player != null)
+            {
+                player.Damage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
+
